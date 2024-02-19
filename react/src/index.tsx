080af157f8b7649fcdda8986ed53c399ba6562ed
gee05053 <gee05053@gmail.com>
2024-02-20 01:10:23 +0900

    feat:implement control column

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -106,7 +106,12 @@ customElements.define(
   reactToWebComponent((props) => {
     return (
       <DefaultProviders {...props}>
-        <UserInfoModal draggable />
+        <UserInfoModal
+          draggable
+          onRequestClose={() => {
+            props.dispatchEvent('close', null);
+          }}
+        />
       </DefaultProviders>
     );
   }),
@@ -117,7 +122,15 @@ customElements.define(
   reactToWebComponent((props) => {
     return (
       <DefaultProviders {...props}>
-        <UserSettingsModal draggable />
+        <UserSettingsModal
+          draggable
+          onRequestOk={() => {
+            props.dispatchEvent('ok', null);
+          }}
+          onRequestClose={() => {
+            props.dispatchEvent('close', null);
+          }}
+        />
       </DefaultProviders>
     );
   }),
